fb5f2ac7b117920956e2b8a8a54261f5553907b8
mtolkacz <michal.tolkacz@gmail.com>
2019-09-23 01:31:34 +0200

    Docker, env and django settings small corrections

--- app/come_together/settings.py
+++ app/come_together/settings.py
@@ -191,5 +191,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 
