aa003445815e51a8b68d8f07c66c29133b9abb0a
mtolkacz <michal.tolkacz@gmail.com>
2020-05-04 21:55:02 +0200

    Added celery methods to process and finish discount during its start/end date
    Added celery methods to process and finish discount during its start/end date - rebuild discount post_save signal method
    Added to process discount method condition if discount is not inactive - added method to discount model
    Added logging to setting - temporarily commented out
    Fixed connection from Celery to database - added env_file in docker-compose to celery services

--- app/gallop/settings.py
+++ app/gallop/settings.py
@@ -263,5 +263,31 @@ CACHES = {
         "KEY_PREFIX": "example"
     }
 }
-
-# sh: 1: cannot create builderror.log: Permission denied
+#
+# LOGGING = {
+#     'version': 1,
+#     # Version of logging
+#     'disable_existing_loggers': False,
+#     # disable logging
+#     # Handlers #############################################################
+#     'handlers': {
+#         'file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': 'gallop-debug.log',
+#         },
+#     ########################################################################
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     # Loggers ##############################################################
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file', 'console'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG')
+#         },
+#     },
+# }
--- app/products/signals.py
+++ app/products/signals.py
@@ -1,18 +1,22 @@
 from .discount import DiscountManager
 from django.core.exceptions import ValidationError
+import logging
+from .tasks import finish_discount, process_discount
+
+logger = logging.getLevelName(__name__)
 
 
 def discount_post_save(sender, instance, signal, *args, **kwargs):
-    # pass
     discount = DiscountManager(instance)
     discount_can_be_processed = discount.check_if_can_be_processed()
 
-    if discount_can_be_processed:
-        discount.process()
-        print('DJANGOTEST: Calculated, {}'.format(instance.id))
-    else:
-        # add celery task to calculate and to close discount at enddate
-        print('DJANGOTEST: Failed, {}'.format(instance.id))
+    # CELERY: Run product discount processing after 2 seconds if can be processed
+    # or add task to run at the discount start date
+    process_discount.apply_async([instance.id], countdown=2) \
+        if discount_can_be_processed else process_discount.apply_async([instance.id], eta=instance.startdate)
+
+    # CELERY: Add task to run discount finish method at the discount end date
+    finish_discount.apply_async([instance.id], eta=instance.enddate)
 
 
 def discount_pre_save(sender, instance, signal, *args, **kwargs):
